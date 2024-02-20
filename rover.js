29476aba3e5189aca3f7141f82517f9e81ade7fb
kalille Overton <>
2024-02-20 16:16:47 -0500

    updated

--- rover.js
+++ rover.js
@@ -1,5 +1,3 @@
-const Message = require('./message.js');
-const Command = require('./command.js');
 
 class Rover {
    constructor(position){
@@ -44,23 +42,5 @@ class Rover {
    }
    
 }
-// let rover3 = new Rover(93823);
-// // console.log(`new rover: ${JSON.stringify(rover3)}`)
-// let commands = [new Command('MOVE', 23423)];
-// // console.log(`2nd log commands ${JSON.stringify(commands[0])},${JSON.stringify(commands[1])}`)
-// let message = new Message('test message with two commands', commands);
-// // console.log(` 3rd log ${JSON.stringify(message.commands.length)}`)
-// rover3.receiveMessage(message)
 
-   let rover = new Rover(100);
-    let commands = [
-       new Command('MOVE', 4321),
-       new Command('STATUS_CHECK'),
-       new Command('MODE_CHANGE', 'LOW_POWER'),
-       new Command('MOVE', 3579),
-       new Command('STATUS_CHECK')
-    ];
-    let message = new Message('TA power', commands);
-    let response = rover.receiveMessage(message);
-    console.log(JSON.stringify(response))
 module.exports = Rover;
