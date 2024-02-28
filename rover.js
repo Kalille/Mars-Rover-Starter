83208b2c5e951cbd0930df4233867ceb7ebe67be
kalille Overton <>
2024-02-28 07:17:31 -0500

    removed forEach method

--- rover.js
+++ rover.js
@@ -1,5 +1,5 @@
-// const Message = require('./message.js');
-// const Command = require('./command.js');
+const Message = require('./message.js');
+const Command = require('./command.js');
 
 class Rover {
    constructor(position){
@@ -43,11 +43,7 @@ class Rover {
       
         
       }
-      results.forEach(result => {
-         if (result.roverStatus) {
-             result.roverStatus = JSON.parse(JSON.stringify(result.roverStatus));
-         }
-     });
+
 
       return { message: message.name, results: results };
    }
@@ -55,16 +51,5 @@ class Rover {
 }
 
 
-// let rover = new Rover(100);
-// let commands = [
-//    new Command('MOVE', 4321),
-//    new Command('STATUS_CHECK'),
-//    new Command('MODE_CHANGE', 'LOW_POWER'),
-//    new Command('MOVE', 3579),
-//    new Command('STATUS_CHECK')
-// ];
-// let message = new Message('TA power', commands);
-// let response = rover.receiveMessage(message);
-// // console.log(response)
-// console.log(JSON.stringify(response, null, 2));
+
 module.exports = Rover;
